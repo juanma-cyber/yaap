f6f1503645263c3c697af0258fe2d968b859b47c
juanma-cyber <psilosybe11@gmail.com>
2023-01-02 16:02:19 +0100

    yapp

---
+++ src/Details.js
@@ -0,0 +1,191 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from "./assets/logo.PNG";
+import logo from "./assets/logo2.PNG";
+// Profile Image
+import profile from "./assets/profile.PNG";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import sass from "./assets/techstack/sass.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import node from "./assets/techstack/node.png";
+import python from "./assets/techstack/python.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+
+// Porject Images
+import projectImage1 from "./assets/projects/project1.jpg";
+import projectImage2 from "./assets/projects/project2.jpg";
+import projectImage3 from "./assets/projects/project3.jpg";
+import projectImage4 from "./assets/projects/project4.jpg";
+import projectImage5 from "./assets/projects/project5.jpg";
+import projectImage6 from "./assets/projects/project6.jpg";
+import projectImage7 from "./assets/projects/project7.jpg";
+import projectImage8 from "./assets/projects/project8.png";
+import projectImage9 from "./assets/projects/project9.jpg";
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Juan",
+  tagline: "Frontend Dev",
+  img: profile,
+  about: `I'm a systems and network technologist, web developer, with more than three years of experience creating websites, online stores, mobile applications, logo design, with JavaScript, HTML5, Tailwind, CSS, Bootstrap, React, php technologies. . , MySQL, Node.js, Wordpress. Graphic design with adobe illustrator, gimp, photoshop, `,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/in/juan-manuel-ram%C3%ADrez-0788511b5//",
+  github: "https://www.github.com/",
+  twitter: "https://twitter.com/",
+  instagram: "https://www.instagram.com/",
+  whatsapp: "https://wa.me/573105161238",
+
+  
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "Frontend Web Developer",
+    Company: `Conservación Colombia`,
+    Location: "Colombia",
+    Type: "Full Time",
+    Duration: "Sep 2019 - Dec 2022",
+  },
+  
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "Graduate",
+    Company: "Udemy, YouTube, Google, Medium",
+    Location: "Online",
+    Type: "Full Time",
+    Duration: "Jan 2022 - Present",
+  },
+  {
+    Position: "Bachelor in Electronics & Communication",
+    Company: `Colegio Ciro Mendía`,
+    Location: "Colombia",
+    Type: "Full Time",
+    Duration: "Dec 1998",
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  redux: redux,
+  sass: sass,
+  tailwind: tailwind,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  node : node,
+  figma: figma,
+  python: python,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Portfolio",
+    image: projectImage1,
+    description: `Suitcase, made with React`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://juanma-cyber.github.io/portfolio/",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "E-commerce",
+    image: projectImage2,
+    description: `E-commerce`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://virtual-shop-ls8bi58n1-juanma-cyber.vercel.app",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Menu",
+    image: projectImage3,
+    description: `React app`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://menu-indol.vercel.app/",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Blog for students",
+    image: projectImage4,
+    description: `Blog made with React`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://blog-kappa-inky.vercel.app/",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Get Links",
+    image: projectImage5,
+    description: `App to save links, React and Firebase`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://storable.vercel.app/",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Invoice Generator",
+    image: projectImage6,
+    description: `CRUD App`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://invoice-generator-jade.vercel.app/",
+    githubLink: "https://github.com",
+
+  },
+  {
+    title: "QuizzApp",
+    image: projectImage9,
+    description: `Qizz App made with React`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://quizz-app-sigma-eight.vercel.app/",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "News Site",
+    image: projectImage7,
+    description: `News Site made with React`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://noti-steel.vercel.app/",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Contact me",
+    image: projectImage8,
+    description: `Contact me`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://wa.me/573105161238",
+    githubLink: "https://github.com",
+  },
+
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "manuram0823@gmail.com",
+  phone: "+573105161238",
+};
